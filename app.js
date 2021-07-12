0b023f51a81326e02287c6ab4dc2897c78a68535
Ray Hiramatsu <ray@Rays-MacBook-Pro.local>
2021-07-11 19:00:22 -0500

    MVP complete

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 //collegiate dictionary my API key: 876921fa-a003-43fe-bb42-c7b41d93a410
 //collegiate thesaurus my API key: 7b7337a7-5b56-4fd4-ae81-be29b4054cf7
-// console.log("hello world");
-
-// $(()=>{
-//     $h1 = $("<h1>").text("asah world").appendTo("body");
-// })
 
 
 const randWordPicker = (arrWords) =>{
@@ -19,7 +14,7 @@ const randWordPicker = (arrWords) =>{
 }
 
 // Pulls AJAX request to API, returns string for adjective1
-const ajaxCaller = ($form) =>{
+const ajaxCaller2 = ($form) =>{
 
     //AJAX request for adjective1
     $.ajax({
@@ -61,18 +56,17 @@ const ajaxCaller = ($form) =>{
 }
 
 
-const ajaxCaller2 = ($form) =>{
-
-        //AJAX request for adjective1
-        return $.ajax({
-            url: `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${$form}?key=7b7337a7-5b56-4fd4-ae81-be29b4054cf7`
-        })
+const ajaxCaller = ($form) =>{
+    return $.ajax({
+        url: `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${$form}?key=7b7337a7-5b56-4fd4-ae81-be29b4054cf7`
+    })
 }
 
 //checks against empty input, invalid (non-word) input
 const inputHandler = (ajaxObj) =>{
         let selectedWord = "";
 
+        //if JSON object is of correct structure
         if(typeof(ajaxObj[0][0]) === "object"){
 
             const arrSyns =(ajaxObj[0][0].meta.syns[0]);
@@ -94,33 +88,35 @@ const inputHandler = (ajaxObj) =>{
 }
 
 
-
 const addModal = (strBandName) =>{
+    let $bandName = $("<p>").text(`strBandName`).appendTo(".modal");
+    $modal.show()
 
+    $bandName.on("click", (event)=>{
+        $modal.hide();
+    });
 }
 
 $(()=>{
-    // let $h1 = $("<h1>").text("asah world").appendTo("body");
-
-
-    // $.ajax({}).then();
-
     $("form").on("submit", (event)=>{
-        event.preventDefault();
         console.log("button was clicked");
+        event.preventDefault();
+
 
         // form variables
-        const $checkboxStatus = $(".chkbox").is(":checked");
+        const $checkboxThe = $(".chkbox").is(":checked");
         const $formAdj1 = $(".adjective1").val();
         const $formAdj2 = $(".adjective2").val();
         const $formNoun1 = $(".noun1").val();
+        const $checkboxPlural = $(".chkbox-plural").is(":checked");
 
-        const $bandName = "";
 
-        console.log(`checkbox status is: ${$checkboxStatus}`);
-        console.log($formAdj1);
-        console.log($formAdj2);
-        console.log($formNoun1);
+
+        console.log(`chkbox status is: ${$checkboxThe}`);
+        console.log(`form adj1 val is: ${$formAdj1}`);
+        console.log(`form adj2 val is: ${$formAdj2}`);
+        console.log(`form noun1 val is: ${$formNoun1}`);
+        console.log(`chkbox-plural status is: ${$checkboxThe}`);
         console.log("//////");
 
         // words to be generated
@@ -129,118 +125,66 @@ $(()=>{
         let randNoun1 = "";
         let randBandName = "";
 
+        // jquery bandname element
+        let $bandName = "";
 
-
-
-
-
-        //AJAX request for adjective1
-        // $.ajax({
-        //     url: `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${$adj1}?key=7b7337a7-5b56-4fd4-ae81-be29b4054cf7`
-        // }).then(
-        //     (data)=>{
-        //         console.log("data is: ");
-        //         console.log(data);
-        //
-        //         console.log(typeof(data));
-        //         console.log(`type of thing returned is: ${typeof(data[0])}`);
-        //
-        //         const blah = typeof(data[0])
-        //
-        //         // console.log(`first string in syns is: ${data[0].meta.syns[0][0]}`);
-        //         console.log("TEST FIRE!");
-        //
-        //         // const arrSynsAdj1 = data[0].meta.syns[0];
-        //         // console.log("arrSyns is: ");
-        //         // console.log(arrSynsAdj1);
-        //
-        //
-        //
-        //         // const randIndex = Math.floor(Math.random() * arrSynsAdj1.length);
-        //         // console.log(`randIndex is: ${randIndex}`);
-        //         //
-        //         // console.log(`random word from arr is ${arrSynsAdj1[randIndex]}`);
-        //
-        //
-        //         if(typeof(data[0]) === "object"){
-        //
-        //             const arrSyns = data[0].meta.syns[0];
-        //             // console.log("arrSyns is: ");
-        //             // console.log(arrSynsAdj1);
-        //
-        //             // const randIndex = Math.floor(Math.random() * arrSynsAdj1.length);
-        //             // console.log(`randIndex is: ${randIndex}`);
-        //             randAdj1 = randWordPicker(data[0].meta.syns[0]);
-        //
-        //
-        //         }
-        //         else{
-        //             console.log("invalid input for adj1! using default...");
-        //             randAdj1 = "big";
-        //         }
-        //
-        //
-        //         console.log("After first ajax request, band name is:")
-        //         console.log(randAdj1);
-        //
-        //
-        //     }
-        // )
-
-
-
-
-        // randAdj1 = ajaxCaller($formAdj1);
-        // console.log(`OUTSIDE ajax request, randAdj1 is : ${randAdj1}`);
-        //
-        // randAdj2 = ajaxCaller($formAdj2);
-        // console.log(`OUTSIDE ajax request, randAdj2 is : ${randAdj2}`);
-        //
-        // randNoun1 = ajaxCaller($formNoun1);
-        // console.log(`OUTSIDE ajax request, randNoun1 is : ${randNoun1}`);
-
-
+        //init modal var
         let $modal = $(".modal");
         // $modal.hide();
 
         //waiting until AFTER ajax requests are been done for adj1, adj2, and noun1
-        $.when(ajaxCaller2($formAdj1), ajaxCaller2($formAdj2), ajaxCaller2($formNoun1)).done(function(a1, a2, n1){
+        $.when(ajaxCaller($formAdj1), ajaxCaller($formAdj2), ajaxCaller($formNoun1)).done(function(a1, a2, n1){
 
+            //reinitialize modal,
             $modal.empty();
             $modal.hide();
 
             // console.log("inside when function");
-            // console.log(a1);
-            // // console.log(a2);
-            // // console.log(n1);
-            //
-            // // console.log(typeof(a1[0]));
-            // console.log(a1[0]);
-            // console.log(typeof(a1[0][0]));
+            console.log(a1);
+            console.log(a2);
+            console.log(n1);
+
 
             randAdj1 = inputHandler(a1);
-            // console.log(`randAdj1, after ajax, after input handler, inside when is: ${randAdj1}`);
-            // console.log(a1[0][0].meta.syns[0][0]);
             randAdj2 = inputHandler(a2);
             randNoun1 = inputHandler(n1);
 
 
             //create band name string
-            randBandName = `${randAdj1} ${randAdj2} ${randNoun1}`;
+            randBandName += `${randAdj1} ${randAdj2} ${randNoun1}`;
 
             //adding "The"
-            if($checkboxStatus){
+            if($checkboxThe){
                 randBandName = `The ${randBandName}`;
+                console.log(`band name, after "The": ${randBandName}`);
             }
-            console.log(`new band name is: ${randBandName}`);
 
 
+            //pluralize noun
+            if($checkboxPlural){
+                //do something here to pluralize noun.
 
-            //edit modal
+                //super basic pluralizer, add 'S'
+                randBandName = `${randBandName}s`;
+                console.log(`band name, after pluralize: ${randBandName}`);
+            }
+
+            console.log(`band name, final: ${randBandName}`);
+
+
+            //add modal, event listeners
+            // addModal(randBandName);
+
+            //add modal
             // let $modal = $(".modal");
-            let $bandName = $("<h3>").text(`new band name is: ${randBandName}`).appendTo(".modal");
-            $modal.show()
+            $bandName = $("<p>").text(`${randBandName}`).appendTo(".modal");
+            let $modalClose = $(".modalClose").text("Close modal by clicking here").appendTo(".modal");
 
+            $modal.show();
+
+
+
+            //modal event listener
             $bandName.on("click", (event)=>{
                 $modal.hide();
             });
--- trash/scratchpad.js
+++ trash/scratchpad.js
@@ -105,3 +105,86 @@ $.ajax({
 
 
 // console.log(arrSynsAdj1);
+
+
+
+@media (min-width: 640px){
+    /* li:nth-child(1){
+        clear:left;
+    }
+    li:nth-child(2){
+        clear: left;
+    } */
+    .first3btn:nth-child(3){
+        clear: left;
+    }
+}
+
+
+
+
+
+//AJAX request for adjective1
+// $.ajax({
+//     url: `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${$adj1}?key=7b7337a7-5b56-4fd4-ae81-be29b4054cf7`
+// }).then(
+//     (data)=>{
+//         console.log("data is: ");
+//         console.log(data);
+//
+//         console.log(typeof(data));
+//         console.log(`type of thing returned is: ${typeof(data[0])}`);
+//
+//         const blah = typeof(data[0])
+//
+//         // console.log(`first string in syns is: ${data[0].meta.syns[0][0]}`);
+//         console.log("TEST FIRE!");
+//
+//         // const arrSynsAdj1 = data[0].meta.syns[0];
+//         // console.log("arrSyns is: ");
+//         // console.log(arrSynsAdj1);
+//
+//
+//
+//         // const randIndex = Math.floor(Math.random() * arrSynsAdj1.length);
+//         // console.log(`randIndex is: ${randIndex}`);
+//         //
+//         // console.log(`random word from arr is ${arrSynsAdj1[randIndex]}`);
+//
+//
+//         if(typeof(data[0]) === "object"){
+//
+//             const arrSyns = data[0].meta.syns[0];
+//             // console.log("arrSyns is: ");
+//             // console.log(arrSynsAdj1);
+//
+//             // const randIndex = Math.floor(Math.random() * arrSynsAdj1.length);
+//             // console.log(`randIndex is: ${randIndex}`);
+//             randAdj1 = randWordPicker(data[0].meta.syns[0]);
+//
+//
+//         }
+//         else{
+//             console.log("invalid input for adj1! using default...");
+//             randAdj1 = "big";
+//         }
+//
+//
+//         console.log("After first ajax request, band name is:")
+//         console.log(randAdj1);
+//
+//
+//     }
+// )
+
+
+
+
+// randAdj1 = ajaxCaller($formAdj1);
+// console.log(`OUTSIDE ajax request, randAdj1 is : ${randAdj1}`);
+//
+// randAdj2 = ajaxCaller($formAdj2);
+// console.log(`OUTSIDE ajax request, randAdj2 is : ${randAdj2}`);
+//
+// randNoun1 = ajaxCaller($formNoun1);
+// console.log(`OUTSIDE ajax request, randNoun1 is : ${randNoun1}`);
